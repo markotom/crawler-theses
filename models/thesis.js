fc59742c427f5faef5aacd7b00cc45d0296e2023
Marco Godínez <markotom@gmail.com>
2017-10-29 23:25:29 -0600

    Change thesis model

--- models/thesis.js
+++ models/thesis.js
@@ -1,22 +1,23 @@
 const mongoose = require('../lib/mongoose')
 const Schema = mongoose.Schema
+const { v4 } = require('uuid')
 
 // Schema (no strict)
 let thesisSchema = new Schema({
-  title: String,
-  author: String,
-  url: String,
-  file: String,
-  degree: String,
-  advisor: String,
-  format: String,
-  legal: String,
-  institution: String,
-  description: String,
-  year: Number
+  uuid: { type: String, default: v4 },
+  author: { type: String },
+  advisor: { type: String },
+  advisor2: { type: String },
+  advisor3: { type: String },
+  degree: { type: String },
+  title: { type: String },
+  year: { type: Number },
+  college: { type: String },
+  institution: { type: String },
+  keywords: { type: Array },
+  rawData: Schema.Types.Mixed
 }, {
-  timestamps: true,
-  strict: false
+  timestamps: true
 })
 
 module.exports = mongoose.model('Thesis', thesisSchema)
