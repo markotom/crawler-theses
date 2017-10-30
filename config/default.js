cb7678cca3c5cc7c870acf7e5c4a2ebaf5467b5c
Marco Godínez <markotom@gmail.com>
2017-10-29 23:24:40 -0600

    Change default config

--- config/default.js
+++ config/default.js
@@ -2,13 +2,7 @@
 
 module.exports = {
   crawling: {
-    url: 'http://oreon.dgbiblio.unam.mx/F',
-    pageLimit: 2,
-    criteria: {
-      degree: 'filosofía',
-      since: '1800',
-      until: '2100'
-    }
+    url: 'http://oreon.dgbiblio.unam.mx/F'
   },
   mongo: {
     port: process.env.MONGO_PORT_27017_TCP_PORT || 27017,
@@ -16,9 +10,5 @@ module.exports = {
     db: process.env.MONGO_PORT_27017_TCP_DATABASE || 'crawlers',
     user: process.env.MONGO_PORT_27017_TCP_USER || '',
     password: process.env.MONGO_PORT_27017_TCP_PASSWORD || ''
-  },
-  redis: {
-    host: process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.0.1',
-    port: process.env.REDIS_PORT_6379_TCP_PORT || 6379
   }
 }
